94a287fda0169277c9b3c7f44fcdcfdd13319d16
OleksandrFilin <acfiln@ukr.net>
2023-07-15 15:25:09 +0300

    task 2

--- js/lesson3.js
+++ js/lesson3.js
@@ -1,4 +1,3 @@
-
 // ЗАДАЧА 1 Напишіть дві функції: letMeSeeYourName(callback) - запитує імя користувача , через prompt і викликає колбек
 // функцію(callback) greet(name) - колбек, який приймає імя і логінує в консоль рядок "Привіт" + імя
 
@@ -10,3 +9,22 @@
 //         console.log(`Привіт ${name}`);
 //     }
 // letMeSeeYourName(greet);
+// ЗАДАЧА 2
+// Напишіть дві функції:
+// makeProduct(name, price, callback) -приймає імя та ціну товару, а також колбек.Функція створює обєкт товару, добавляє йому унікальний ідентифікатор, в значення id і викликає колбек, передаючи йому створений обєкт.
+// showProduct(product) - колбек приймає обєкт продукта і логінує його в консоль;
+// makeProduct("Холодильник", 10000, logger);
+
+function makeProduct(name, price, collback) {
+  const good = {
+    name,
+    price,
+    id: Date.now(),
+  };
+  collback(good);
+}
+function showProduct(product) {
+  console.log(product);
+}
+
+makeProduct("Холодильник", 10000, showProduct);
